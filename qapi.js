69adb3c74eea72972cd560720c2660344fd0fe56
ansidti <74551950+ansidti@users.noreply.github.com>
2020-11-16 21:53:31 +0700

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb2848ab1c8af001eb538ea",
+				"5fb2848ab1c8af001eb538ec"
+			]
+		},
+		"5fb2848ab1c8af001eb538ea": {
+			"id": "5fb2848ab1c8af001eb538ea",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb2848ab1c8af001eb538ec": {
+			"id": "5fb2848ab1c8af001eb538ec",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
